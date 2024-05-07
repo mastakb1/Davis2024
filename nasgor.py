fa91f5d2c20801c7df766ea1d922f62c2a46d89a
mastakb1 <99850986+mastakb1@users.noreply.github.com>
2024-05-07 14:30:57 +0700

    Update nasgor.py

--- nasgor.py
+++ nasgor.py
@@ -1,1 +1,5 @@
-
+import requests 
+from bs4 import BeautifulSoup 
+import pandas as pd 
+# headers = {"cookie": "CONSENT=YES+cb.20240430-04-p0.en+FX+908"
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0;  Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'} 
